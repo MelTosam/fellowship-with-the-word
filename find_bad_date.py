03cb93eb1c6b7637a1ffc8a747d889cf97704925
MelTosam <tosammelvis@gmail.com>
2026-07-02 10:57:01 +0100

    Hide future devotionals, add delete feature, fix date sorting

---
+++ find_bad_date.py
@@ -0,0 +1,13 @@
+﻿import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = psycopg2.connect(os.environ.get('DATABASE_URL'))
+cursor = connection.cursor()
+cursor.execute("SELECT id, title, date FROM devotionals WHERE date = 'o4-08-2026'")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+connection.close()
---
+++ fix_bad_date.py
@@ -0,0 +1,13 @@
+﻿import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection = psycopg2.connect(os.environ.get('DATABASE_URL'))
+cursor = connection.cursor()
+cursor.execute("UPDATE devotionals SET date = '04-08-2026' WHERE id = 15")
+connection.commit()
+cursor.close()
+connection.close()
+print('Fixed')
