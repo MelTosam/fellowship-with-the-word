a6d077f289faf08299f7cba085eb7038cfb375b7
MelTosam <tosammelvis@gmail.com>
2026-07-24 12:01:25 +0100

    Restore Fiverr light burst icon

--- warmup.py
+++ warmup.py
@@ -1,19 +1,19 @@
-words = ["prayer", "faith", "go", "sanctification", "love", "word"]
+words = ["grace", "redemption", "joy", "justification", "peace", "truth"]
 
 def filter_by_length(words, minimum):
     result = []
     for word in words:
-        if len(word) > minimum:
+        if len(word) >minimum:
             result.append(word)
-    return result 
+    return result
 result = filter_by_length(words, 5)
-print(result)
+print(result)  
 
 def find_longest(words):
     winner = words[0]
     for word in words:
-        if len(word) > len(winner):
+        if len(word) >len(winner):
             winner = word
     return winner
+print(find_longest(words))        
 
-print(find_longest(words))       
