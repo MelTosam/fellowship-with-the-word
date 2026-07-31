169b9bb22cacd9a1d3de201a2f6833babffea4d4
MelTosam <tosammelvis@gmail.com>
2026-07-31 11:44:33 +0100

    Add progress tracker and reading history

---
+++ create_reading_history.py
@@ -0,0 +1,20 @@
+﻿import psycopg2
+
+NEON_URL = "postgresql://neondb_owner:npg_X4Ihzl2tAdpO@ep-raspy-king-atkm989g.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require"
+
+conn = psycopg2.connect(NEON_URL)
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS reading_history (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        devotional_id INTEGER NOT NULL,
+        read_date TEXT NOT NULL
+    )
+''')
+
+conn.commit()
+cursor.close()
+conn.close()
+print('Reading history table created')
