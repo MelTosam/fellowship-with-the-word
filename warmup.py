03d0f1aa2f17d529d0fccd2343a46e81cf8f01f4
MelTosam <tosammelvis@gmail.com>
2026-08-05 20:49:24 +0100

    Update to new professional FW logo

--- warmup.py
+++ warmup.py
@@ -1,5 +1,29 @@
-devotional = {"title": "Putting on Christ", "date": "01-08-2026"}
+numbers = [12, 5, 28, 3, 45, 17, 19]
 
-def describe_devotional(devotional):
-    return f"title: {devotional["title"]} | date: {devotional["date"]}"
-print(describe_devotional(devotional))
+def filter_above(numbers, minimum):
+    result = []
+    for number in numbers:
+        if number > minimum:
+            result.append(number)
+    return result
+result = filter_above(numbers, 10)
+print(result)
+
+def find_highest(numbers):
+    winner = numbers[0]
+    for number in numbers:
+        if number > winner:
+            winner = number
+    return winner
+
+winner = find_highest(numbers)
+print(winner)  
+
+def find_lowest_above(numbers, minimum):
+    lowest = []
+    for number in numbers:
+        if number > minimum:
+            lowest.append(number)
+    return lowest
+lowest = find_lowest_above(numbers)
+print(lowest)          
